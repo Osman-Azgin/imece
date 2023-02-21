0368d55a81e2412a793f323d4b72a7e50cfa2863
Osman-Azgin <osman.azgin.17@gmail.com>
2023-02-21 19:03:12 +0300

    UI Fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,20 +55,50 @@
         <h1 class="mt-6 text-xl text-center">{{ __('Requirements') }}</h1>
         <div class="mt-6" style="min-height: 60vh;">
             @foreach($currentRequirements as $requirement)
-                <div class="w-full bg-white p-4 md:p-8 grid grid-cols-4 gap-4 rounded-lg justify-center items-center">
-                    <div class="col-span-1 flex">
-                        <svg style="width: 50px;color: purple;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="50 50" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-                        </svg>
+            {{--
+            <div class="w-full bg-white p-4 md:p-8 grid grid-cols-4 gap-4 rounded-lg justify-center items-center">
+                <div class="col-span-1 flex">
+                    <svg style="width: 50px;color: purple;" xmlns="http://www.w3.org/2000/svg" fill="none"
+                         viewBox="50 50" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"/>
+                    </svg>
 
+                    {{ $requirement->inkindDonation->name }}
+                </div>
+                <div class="col-span-1">
+                    {{ $requirement->warehouse->team->name }}
+                </div>
+                <div class="col-span-2 text-right">
+                    {{ $requirement->warehouse->address->district->name }}
+                    /{{ $requirement->warehouse->address->city->name }}
+                </div>
+            </div> --}}
+                <div class="w-full p-3 md:p-6 grid grid-cols-4 md:grid-cols-5 gap-4 md:gap-2 bg-white rounded-lg items-center justify-center pl-1 mb-6">
+                    <div class="flex items-start justify-start h-full col-span-2">
+                        <svg style="width: 40px;height:40px;color:purple;margin-right: 15px;"
+                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                             stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"/>
+                        </svg>
                         {{ $requirement->inkindDonation->name }}
                     </div>
-                    <div class="col-span-1">
-                        {{ $requirement->warehouse->team->name }}
+                    <div class="flex items-start justify-start h-full col-span-2 md:col-span-1">
+                        <svg style="width: 30px;height:30px;color:purple;margin-right: 15px;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                        </svg>
+                        <div>
+                            {{ $requirement->warehouse->team->name }}<br>
+                            {{ $requirement->warehouse->name }} Deposu
+                        </div>
                     </div>
-                    <div class="col-span-2 text-right">
-                        {{ $requirement->warehouse->address->district->name }}
-                        /{{ $requirement->warehouse->address->city->name }}
+                    <div class="flex justify-between md:justify-end h-full col-span-4 md:col-span-2 pl-2">
+                        <div>
+                            {{ $requirement->warehouse->address->street->name }}<br>
+                            {{ $requirement->warehouse->address->district->name }}
+                            /{{ $requirement->warehouse->address->city->name }}
+                        </div>
                     </div>
                 </div>
             @endforeach
